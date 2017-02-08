d0d7b8d9a64b5cea5964b36e1992926f85be10bb
Dan Gopstein <dan@gopstein.com>
2017-02-08 02:37:43 -0500

    Include js in yml

---
+++ _plugins/inclusion_data_reader.rb
@@ -0,0 +1,57 @@
+module Jekyll
+  class DataReader
+    include Convertible
+    attr_accessor :data, :content
+    attr_accessor :site, :context
+    def read_data_file_with_inclusion(path)
+        puts "\n\n !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n"
+      #begin
+        dir = File.dirname(path)
+        filename = File.basename(path)
+        content = File.read(@path || site.in_source_dir(dir, filename))
+        puts "dir, filename: #{[dir, filename].inspect}"
+
+        processed_filename = 
+          if contains_frontmatter?(content)
+            scrubbed_content = without_frontmatter(content)
+
+            @site = Jekyll::Site.new(
+              Jekyll.configuration({
+                                     "source"      => '',
+                                     "destination" => '_site'}))
+            @context = Liquid::Context.new({}, {}, { :site => @site })
+            template = Liquid::Template.parse(scrubbed_content)
+            rendered = template.render(@context, "foo" => 'abc')
+
+            puts "RENDERED:"
+            puts rendered
+            puts"====="
+            filename
+          else
+            filename
+          end
+      #rescue => e
+      #  Jekyll.logger.warn "SSL-specific error reading file #{filename}: #{e.message}"
+      #end
+        puts "\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n"
+
+      read_data_file_without_liquid(path)
+    end
+
+    alias_method :read_data_file_without_liquid, :read_data_file
+    alias_method :read_data_file, :read_data_file_with_inclusion
+  end
+end
+
+def contains_frontmatter?(content)
+    !!(content =~ Jekyll::Document::YAML_FRONT_MATTER_REGEXP)
+end
+
+def without_frontmatter(content)
+    match = Jekyll::Document::YAML_FRONT_MATTER_REGEXP.match(content)
+    if match
+      match.post_match
+    else
+      content
+    end
+end
