ac0e5d1d76934f5115e2542c33bd158f9a656942
Dan Gopstein <dan@gopstein.com>
2017-02-08 19:38:54 -0500

    Site works with templating

--- _plugins/inclusion_data_reader.rb
+++ _plugins/inclusion_data_reader.rb
@@ -4,6 +4,7 @@ module Jekyll
     attr_accessor :data, :content
     attr_accessor :site, :context
     def read_data_file_with_inclusion(path)
+      res = nil
         puts "\n\n !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n"
         site = Jekyll.sites.last # If there are multiple sites this might be the wrong one
       #begin
@@ -12,35 +13,39 @@ module Jekyll
         content = File.read(@path || site.in_source_dir(dir, filename))
         puts "dir, filename: #{[dir, filename].inspect}"
 
-        processed_filename = 
-          if contains_frontmatter?(content)
-            scrubbed_content = without_frontmatter(content)
+        #processed_filename = 
+        #  if contains_frontmatter?(content)
+        #    scrubbed_content = without_frontmatter(content)
 
             context = Liquid::Context.new({}, {}, { :site => site })
-            template = Liquid::Template.parse(scrubbed_content)
+            #template = Liquid::Template.parse(scrubbed_content)
+            template = Liquid::Template.parse(content)
             rendered = template.render(context)
 
             puts "RENDERED:"
-            puts rendered
-            puts"====="
-            filename
+            #puts rendered
 
             Dir.mktmpdir do |dir|
               tmppath = File.join(dir, filename)
 
+              puts "tmppath: #{tmppath.inspect}"
               File.write(tmppath, rendered)
+              puts "File.read: #{File.read(tmppath)[0..200]}"
 
-              read_data_file_without_liquid(tmppath)
+              res = read_data_file_without_liquid(tmppath)
+              puts "after read_data"
             end
-          else
-            read_data_file_without_liquid(path)
-          end
+            puts"====="
+          #else
+          #  read_data_file_without_liquid(path)
+          #end
       #rescue => e
       #  Jekyll.logger.warn "SSL-specific error reading file #{filename}: #{e.message}"
       #end
         puts "\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n"
 
-      read_data_file_without_liquid(path)
+      #read_data_file_without_liquid(path)
+        res
     end
 
     alias_method :read_data_file_without_liquid, :read_data_file
